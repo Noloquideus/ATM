11528d6967da575fc96fd870482327b336b0ae60
Noloquideus <daniilmanukian@gmail.com>
2023-12-27 00:14:30 +0300

    feat: initializing feat: tools for working with database

---
+++ database/utils/__init__.py
@@ -0,0 +1,5 @@
+from database.utils.users_table import UserDatabaseManager
+from database.utils.cards_table import CardDatabaseManager
+
+user_database_manager = UserDatabaseManager()
+card_database_manager = CardDatabaseManager()
